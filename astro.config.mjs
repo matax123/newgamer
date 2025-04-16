56c56e7819481630c16ebbaa02eaf27adf56dd12
Matías Ortiz <matiasortizurrutia0@gmail.com>
2025-04-16 11:02:02 -0400

    deploy configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,7 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-  },
-  vite: {
-    preview: {
-      host: '0.0.0.0',
-      port: 4321,
-      strictPort: false,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-      proxy: {},
-      cors: true,
-      hmr: {
-        host: '0.0.0.0',
-      },
-    },
-    server: {
-      host: '0.0.0.0',
-      port: 4321,
-      strictPort: false,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-      proxy: {},
-      cors: true,
-      hmr: {
-        host: '0.0.0.0',
-      },
-    },
-  },
-});
+    site: 'https://newgamer.cl'
+  })
