74cc38866a5c8a9c36fe756384624cfa7318cff9
Matías Ortiz <matiasortizurrutia0@gmail.com>
2025-09-07 19:09:26 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     vite: {
-      server: {
+      preview: {
         allowedHosts: ['newgamer.cl'],
       },
     },
